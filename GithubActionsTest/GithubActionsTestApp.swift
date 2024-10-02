695079a82db623883437f52dd7bc3bea4465db21
Anton Kaliuzhnyi <m.a.r.kalyuzhny@gmail.com>
2024-10-02 17:33:37 +0300

    Setup

---
+++ GithubActionsTest/GithubActionsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsTestApp.swift
+//  GithubActionsTest
+//
+//  Created by Anton Kaliuzhnyi on 02.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GithubActionsTestTests/GithubActionsTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsTestTests.swift
+//  GithubActionsTestTests
+//
+//  Created by Anton Kaliuzhnyi on 02.10.2024.
+//
+
+import XCTest
+@testable import GithubActionsTest
+
+final class GithubActionsTestTests: XCTestCase {
+    
+    func testExample() throws {
+        XCTAssert(2*2 == 4)
+    }
+    
+}
